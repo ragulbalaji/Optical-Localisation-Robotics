fcbd1ca1c3473c64c6688c6fbf8ddd6cb23c3775
Ragul Balaji <ragulbaby@gmail.com>
2018-01-28 04:06:27 +0800

    cool demos

--- ARProcessNode/index.js
+++ ARProcessNode/index.js
@@ -21,12 +21,30 @@ const io = require('socket.io')(httpsServer)
 const path = require('path')
 
 const net = require('net')
-const s = new net.Socket()
-//s.connect(8080, '192.168.43.177', () => {
-//  s.write('HELLO FROM NODE')
-//})
+
+const oID = {
+	"Robot": "28",
+	"F24": "24",
+	"F25": "25"
+}
 
 let recentFrame = {};
+
+setInterval(mainLoop, 100);
+
+function mainLoop() {
+	if (isDefined(recentFrame[oID["Robot"]]) && isDefined(recentFrame[oID["F24"]])) { //MIRROR ROLL OF F24
+		robotAngle = parseFloat(recentFrame[oID["Robot"]].geometry.roll)
+		f24Angle = parseFloat(recentFrame[oID["F24"]].geometry.roll)
+		goal = parseInt(-3 * (robotAngle - f24Angle));
+		if (Math.abs(goal) > 0) sendNXT("s," + goal.toString())
+	} else if (isDefined(recentFrame[oID["Robot"]]) && isDefined(recentFrame[oID["F25"]])) { //NO WORK
+		//robotAngle = parseFloat(recentFrame[oID["Robot"]].geometry.roll)
+		//goal = Math.round(3 * Math.sin(Date.now() / 500)) 
+		//console.log(goal);
+		//if (Math.abs(goal) > 0) sendNXT("s," + goal.toString())
+	}
+}
 
 io.on('connection', socket => {
 	console.log(`${socket.id} connected.`)
@@ -68,7 +86,44 @@ leapController.connect()
 setInterval(function () { //Stats Loop
 	for (var key of Object.keys(recentFrame)) {
 		var marker = recentFrame[key];
-		if (marker.time < Date.now() - 10000) delete recentFrame[key];
+		if (marker.time < Date.now() - 100) delete recentFrame[key];
 	}
 	console.log(Object.keys(recentFrame))
 }, 2000)
+
+var sendBlocker = false;
+
+function sendNXT(msg) {
+	if (sendBlocker) {
+		return "Busy";
+	} else {
+		sendBlocker = true
+		const nxt = new net.Socket()
+		try {
+			console.log("Attempt Send")
+			nxt.connect(8080, '192.168.43.177', (err) => {
+				if (err !== undefined) {
+					sendBlocker = false
+					console.log("DED", err)
+					return;
+				}
+				console.log("Sending.. ", msg)
+				nxt.write(msg)
+				nxt.end()
+				sendBlocker = false
+			})
+		} catch (e) {
+			next(e)
+		}
+	}
+}
+
+function isDefined(obj) {
+	return !(obj === undefined);
+}
+
+function getAngle(x1, y1, x2, y2) {
+	var dx = x1 - x2,
+		dy = y1 - y2;
+	return Math.atan2(dy, dx);
+};
